a521e86f2278a6bc5750d9419c10e6b1c7ffeba6
pder <1198811801@qq.com>
2022-03-10 18:36:59 +0800

    init

--- type-challenges/12-medium-chainable-options/template.ts
+++ type-challenges/12-medium-chainable-options/template.ts
@@ -1,4 +1,5 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<options = {} > = {
+  option<K extends string, V extends any>(key: Exclude<K, keyof options>, value: V): 
+  Chainable<options & Record<K, V>>
+  get(): options
 }
